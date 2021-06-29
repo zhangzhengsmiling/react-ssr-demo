2f472e9545fd7f95452c00a3c98f5cf6a0dd40a0
zhangzhengsmiling <zhangzhengsmiling@163.com>
2021-06-29 09:35:56 +0800

    fix: add node_modules to bundle for server-side bundling
    - 服务端打包node_modules中的包到bundle里

--- config/webpack.server.js
+++ config/webpack.server.js
@@ -7,18 +7,21 @@ const webpackConfigClient = {
   target:'node',
   entry: path.resolve(__dirname, '../server/index.js'),
   output: {
-    filename:'bundle.js',
+    filename:'server.js',
     path:path.resolve(__dirname,'../build')
   },
   // webpack-node-externals的目的是为了防止node_modules目录下的第三方模块被打包进去
   // 因为nodejs默认会去node_modules目录下去寻找和使用第三方模块。
-  externals: [nodeExternals()],  //保持node中require的引用方式
+  // externals: [nodeExternals({
+  //   allowlist: [
+  //     /^antd/
+  //   ]
+  // })],  //保持node中require的引用方式
   // plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.less?$/,
-        // include: /node_modules/,
         use: ['isomorphic-style-loader', {
           loader: 'css-loader',
           options: {
